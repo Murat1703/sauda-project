0355d5c5e859cd0abe2ee01f84b2216abf39453a
family <family@MacBook-Air-family.local>
2026-05-21 10:18:44 +0500

    working on product page mobile

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -17,6 +17,7 @@ import 'swiper/css/navigation';
 import 'swiper/css/thumbs';
 import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
 import { useMediaQuery } from 'react-responsive';
+import { Button } from '../../components/Button';
 
 
 
@@ -123,7 +124,7 @@ export const ProductPage = () =>{
                                     slidesPerView={4}
                                     freeMode={true}
                                     watchSlidesProgress={true}
-                                    modules={[FreeMode, Navigation, Thumbs]}
+                                    modules={[FreeMode, Thumbs]}
                                     className="mySwiper"
                                 >
                                     <SwiperSlide>
@@ -581,7 +582,7 @@ export const ProductPage = () =>{
                                     }}
                                     pagination={true}
                                     spaceBetween={10}
-                                    modules={[FreeMode, Navigation, Thumbs]}
+                                    modules={[FreeMode]}
                                 >
                                     <SwiperSlide>
                                     <img src="https://swiperjs.com/demos/images/abstract-1.jpg" />
@@ -653,7 +654,149 @@ export const ProductPage = () =>{
                         <Title>{product?.title}</Title>
                         <span>Код: {product?.id}</span>
                     </div>
+                    <div className={cls.mobileProductPriceBlock}>
+                        <div className={cls.mobileProductPriceValue}>
+                            {product?.oldPrice && <div className={cls.oldPrice}><p>{product?.oldPrice} ₸</p>
+                            <Badge type="discount">-{product?.discountPercent}%</Badge></div>}
+                            <p className={cls.mobileFinalPrice}>{product?.finalPrice} ₸</p>
+                        </div>
+                        <div className={cls.mobileProductActionBtn}>
+                            <Button >
+                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+                                <path d="M12.0003 6.75V4.5C12.0003 2.84315 10.6571 1.5 9.00028 1.5C7.34342 1.5 6.00028 2.84315 6.00028 4.5V6.75M2.69428 7.76397L2.24428 12.564C2.11633 13.9287 2.05236 14.6111 2.27881 15.1382C2.47775 15.6012 2.82637 15.984 3.26879 16.2253C3.77242 16.5 4.4578 16.5 5.82856 16.5H12.172C13.5428 16.5 14.2281 16.5 14.7318 16.2253C15.1742 15.984 15.5228 15.6012 15.7217 15.1382C15.9482 14.6111 15.8842 13.9287 15.7563 12.564L15.3063 7.76397C15.1982 6.61151 15.1442 6.03528 14.885 5.59962C14.6568 5.21594 14.3195 4.90883 13.9162 4.71738C13.4583 4.5 12.8795 4.5 11.722 4.5L6.27856 4.5C5.12104 4.5 4.54229 4.5 4.08434 4.71738C3.68103 4.90883 3.34378 5.21594 3.11552 5.59962C2.85634 6.03528 2.80232 6.61151 2.69428 7.76397Z" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+                                <p>В корзину</p>
+                            </Button>
+                        </div>
+                        <div className={cls.mobileProductDeliveryItems}>
+                            <div className={cls.productDeliveryBlock}>
+                                <div className={cls.productDeliveryItem}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                                    <path d="M13.3337 11.1454C16.2768 11.724 18.3337 13.0456 18.3337 14.5833C18.3337 16.6544 14.6027 18.3333 10.0003 18.3333C5.39795 18.3333 1.66699 16.6544 1.66699 14.5833C1.66699 13.0456 3.72382 11.724 6.66699 11.1454M10.0003 14.1667V7.5M10.0003 7.5C11.381 7.5 12.5003 6.38071 12.5003 5C12.5003 3.61929 11.381 2.5 10.0003 2.5C8.61961 2.5 7.50033 3.61929 7.50033 5C7.50033 6.38071 8.61961 7.5 10.0003 7.5Z" stroke="#909596" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+                                    </svg>
+                                    <div className={cls.productDeliveryItemText}>
+                                        <p>Самовывоз: сегодня из <span>3 магазинов</span></p>
+                                        <span>Бесплатно</span>
+                                    </div>
+                                </div>
+                                <div className={cls.productDeliveryItem}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                                    <path d="M11.667 5.83331H13.6147C13.8185 5.83331 13.9204 5.83331 14.0164 5.85634C14.1014 5.87675 14.1827 5.91042 14.2572 5.95611C14.3413 6.00765 14.4134 6.07971 14.5575 6.22384L17.9431 9.60946C18.0873 9.75358 18.1593 9.82564 18.2109 9.90974C18.2565 9.9843 18.2902 10.0656 18.3106 10.1506C18.3337 10.2465 18.3337 10.3484 18.3337 10.5523V12.9166C18.3337 13.3049 18.3337 13.4991 18.2702 13.6522C18.1856 13.8564 18.0234 14.0186 17.8192 14.1032C17.6661 14.1666 17.4719 14.1666 17.0837 14.1666M12.917 14.1666H11.667M11.667 14.1666V5.99998C11.667 5.06656 11.667 4.59985 11.4853 4.24333C11.3255 3.92973 11.0706 3.67476 10.757 3.51497C10.4005 3.33331 9.93375 3.33331 9.00033 3.33331H4.33366C3.40024 3.33331 2.93353 3.33331 2.57701 3.51497C2.2634 3.67476 2.00844 3.92973 1.84865 4.24333C1.66699 4.59985 1.66699 5.06656 1.66699 5.99998V12.5C1.66699 13.4205 2.41318 14.1666 3.33366 14.1666M11.667 14.1666H8.33366M8.33366 14.1666C8.33366 15.5474 7.21437 16.6666 5.83366 16.6666C4.45295 16.6666 3.33366 15.5474 3.33366 14.1666M8.33366 14.1666C8.33366 12.7859 7.21437 11.6666 5.83366 11.6666C4.45295 11.6666 3.33366 12.7859 3.33366 14.1666M17.0837 14.5833C17.0837 15.7339 16.1509 16.6666 15.0003 16.6666C13.8497 16.6666 12.917 15.7339 12.917 14.5833C12.917 13.4327 13.8497 12.5 15.0003 12.5C16.1509 12.5 17.0837 13.4327 17.0837 14.5833Z" stroke="#909596" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+                                    </svg>
+                                    <div className={cls.productDeliveryItemText}>
+                                        <p>Доставка: сегодня, {today}</p>
+                                        <span>Бесплатно в квадрате доставки</span>
+                                    </div>
+                                </div>
+                                <div className={cls.productDeliveryItem}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                                    <path d="M10.8325 9.16659L2.91588 17.0833M11.6812 2.94865C12.6962 3.62215 13.6718 4.41617 14.5835 5.32783C15.503 6.24735 16.3029 7.23193 16.9801 8.25638M7.71168 6.58004L5.31596 5.78147C5.04005 5.6895 4.73616 5.74802 4.51415 5.93588L2.13318 7.95054C1.64574 8.36299 1.78423 9.14791 2.38338 9.36865L4.63938 10.1998M9.73342 15.2937L10.5646 17.5497C10.7853 18.1488 11.5702 18.2873 11.9827 17.7999L13.9973 15.4189C14.1852 15.1969 14.2437 14.893 14.1518 14.6171L13.3532 12.2214M16.123 1.8922L12.0343 2.57365C11.5928 2.64723 11.1878 2.86424 10.8821 3.1911L5.37118 9.08205C3.94272 10.609 3.98245 12.9937 5.46099 14.4722C6.93953 15.9508 9.32418 15.9905 10.8512 14.562L16.7421 9.05114C17.069 8.74537 17.286 8.34041 17.3596 7.89891L18.041 3.81018C18.229 2.68215 17.2511 1.70419 16.123 1.8922Z" stroke="#909596" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+                                    </svg>
+                                    <div className={cls.productDeliveryItemText}>
+                                        <p>Экспресс-доставка: сегодня, {today}</p>
+                                        <span>от 5 200 ₸</span>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
+                <div className={cls.mobileProductDescriptionBlock}>
+                    <div className={cls.mobileProductDescriptionType}>
+                        <button
+                            onClick={()=>setActive(0)}
+                            className={active == 0 ? `${cls.active}`: null}
+                        >
+                            <p>Характеристики</p>
+                        </button>
+                        <button 
+                            onClick={()=>setActive(1)}
+                            className={active == 1 ? `${cls.active}`: null}
+                        >
+                            <p>Описание</p>
+                        </button>
+                    </div>
+                    <div className={cls.mobileProductDescriptionWrapper}>
+                        {active == 0 && 
+                            <div className={cls.text}>
+                                <div className={cls.textItem}>
+                                    <span>Бренд</span>
+                                    <div className={cls.line}></div>
+                                    <p>{product?.brand}</p>
+                                </div>
+                                <div className={cls.textItem}>
+                                    <span>Напряжение аккумулятора, В</span>
+                                    <div className={cls.line}></div>
+                                    <p>20</p>
+                                </div>
+                                <div className={cls.textItem}>
+                                    <span>Максимальный крутящий момент, Н/м</span>
+                                    <div className={cls.line}></div>
+                                    <p>40</p>
+                                </div>
+                                <div className={cls.textItem}>
+                                    <span>Количество скоростей работы</span>
+                                    <div className={cls.line}></div>
+                                    <p>1350</p>
+                                </div>
+                                <div className={cls.textItem}>
+                                    <span>Тип двигателя 
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                                        <g clip-path="url(#clip0_1020_761)">
+                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M8.00007 2.32801C4.86751 2.32801 2.32807 4.86745 2.32807 8.00001C2.32807 11.1326 4.86751 13.672 8.00007 13.672C11.1326 13.672 13.6721 11.1326 13.6721 8.00001C13.6721 4.86745 11.1326 2.32801 8.00007 2.32801ZM0.87207 8.00001C0.87207 4.06332 4.06338 0.872009 8.00007 0.872009C11.9368 0.872009 15.1281 4.06332 15.1281 8.00001C15.1281 11.9367 11.9368 15.128 8.00007 15.128C4.06338 15.128 0.87207 11.9367 0.87207 8.00001ZM7.27207 5.44001C7.27207 5.03795 7.59801 4.71201 8.00007 4.71201H8.00647C8.40853 4.71201 8.73447 5.03795 8.73447 5.44001C8.73447 5.84207 8.40853 6.16801 8.00647 6.16801H8.00007C7.59801 6.16801 7.27207 5.84207 7.27207 5.44001ZM8.00007 7.27201C8.40213 7.27201 8.72807 7.59795 8.72807 8.00001V10.56C8.72807 10.9621 8.40213 11.288 8.00007 11.288C7.59801 11.288 7.27207 10.9621 7.27207 10.56V8.00001C7.27207 7.59795 7.59801 7.27201 8.00007 7.27201Z" fill="#9DA5B2"/>
+                                        </g>
+                                        <defs>
+                                            <clipPath id="clip0_1020_761">
+                                            <rect width="16" height="16" fill="white"/>
+                                            </clipPath>
+                                        </defs>
+                                        </svg>
+
+                                    </span>
+                                    <div className={cls.line}></div>
+                                    <p>Щеточный</p>
+                                </div>
+                                <div className={cls.textItem}>
+                                    <span>
+                                        Тип патрона 
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                                        <g clip-path="url(#clip0_1020_761)">
+                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M8.00007 2.32801C4.86751 2.32801 2.32807 4.86745 2.32807 8.00001C2.32807 11.1326 4.86751 13.672 8.00007 13.672C11.1326 13.672 13.6721 11.1326 13.6721 8.00001C13.6721 4.86745 11.1326 2.32801 8.00007 2.32801ZM0.87207 8.00001C0.87207 4.06332 4.06338 0.872009 8.00007 0.872009C11.9368 0.872009 15.1281 4.06332 15.1281 8.00001C15.1281 11.9367 11.9368 15.128 8.00007 15.128C4.06338 15.128 0.87207 11.9367 0.87207 8.00001ZM7.27207 5.44001C7.27207 5.03795 7.59801 4.71201 8.00007 4.71201H8.00647C8.40853 4.71201 8.73447 5.03795 8.73447 5.44001C8.73447 5.84207 8.40853 6.16801 8.00647 6.16801H8.00007C7.59801 6.16801 7.27207 5.84207 7.27207 5.44001ZM8.00007 7.27201C8.40213 7.27201 8.72807 7.59795 8.72807 8.00001V10.56C8.72807 10.9621 8.40213 11.288 8.00007 11.288C7.59801 11.288 7.27207 10.9621 7.27207 10.56V8.00001C7.27207 7.59795 7.59801 7.27201 8.00007 7.27201Z" fill="#9DA5B2"/>
+                                        </g>
+                                        <defs>
+                                            <clipPath id="clip0_1020_761">
+                                            <rect width="16" height="16" fill="white"/>
+                                            </clipPath>
+                                        </defs>
+                                        </svg>
+                                    </span>
+                                    <div className={cls.line}></div>
+                                    <p>Быстрозажимной</p>
+                                </div>
+                                <div className={cls.textItem}>
+                                    <span>Диаметр зажимаемой оснастки</span>
+                                    <div className={cls.line}></div>
+                                    <p>0.8-10 мм</p>
+                                </div>
+                                <div className={cls.textItem}>
+                                    <span>Тип аккумулятора</span>
+                                    <div className={cls.line}></div>
+                                    <p>Li-Ion</p>
+                                </div>
+                                <div className={cls.textItem}>
+                                    <span>Код товара</span>
+                                    <div className={cls.line}></div>
+                                    <p>{product?.id}</p>
+                                </div>
+                                <div>
+                                    <a href="" className={cls.link}>Все характеристики</a>
+                                </div>
+                            </div>
+                        }
+                    </div>
+                </div>
+
             </div>
             }
         </div>
